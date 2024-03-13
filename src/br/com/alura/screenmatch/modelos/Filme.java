0ca5057be1674b2a7608a2bfd6977b2fb3e0ca62
Filipe <fbianchi.andrade@gmail.com>
2024-03-13 16:09:04 -0300

    modificando classes para importar a lig Gson para converter JSON para String e String para JSON

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,29 +1,42 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 import br.com.alura.screenmatch.calculos.Classificavel;
 
 public class Filme extends Titulo implements Classificavel {
+    @SerializedName("Director")
     private String diretor;
-
+    @SerializedName("Plot")
+    private String descricao;
+    
+    
     public Filme(String nome, int anoDeLancamento) {
         super(nome, anoDeLancamento);
     }
-
+    
     public String getDiretor() {
         return diretor;
     }
-
+    
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+    
+    public String getDescricao() {
+        return descricao;
     }
 
     @Override
     public int getClassificacao() {
         return (int) pegaMedia() / 2;
     }
-
+    
     @Override
     public String toString() {
-        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
+        String stringoDoFilme = 
+        "Nome: %s; Ano de lançamento: %d; Diretor: %s; Descrição: %s"
+        .formatted(this.getNome(), this.getAnoDeLancamento(), this.getDiretor(), this.getDescricao());
+        return stringoDoFilme;
     }
 }
--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.principal;
 
-import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.Filme;
+
 import com.google.gson.Gson;
 
 import java.io.IOException;
@@ -15,8 +16,9 @@ public class PrincipalComBusca {
         Scanner leitura = new Scanner(System.in);
         System.out.println("Digite um filme para busca: ");
         var busca = leitura.nextLine();
+        leitura.close();
 
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=coloque_sua_apikey";
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=975cb0b4";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
@@ -26,10 +28,9 @@ public class PrincipalComBusca {
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
         String json = response.body();
-        System.out.println(json);
 
         Gson gson = new Gson();
-        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
-        System.out.println(meuTitulo);
+        Filme meuFilme = gson.fromJson(json, Filme.class);
+        System.out.println(meuFilme);
     }
 }
